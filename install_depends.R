c22e703a281ab6b0885766f7b76332db56ee860d
Frank <fbearoff@users.noreply.github.com>
2025-06-23 11:26:21 -0400

    add dependency installation script

---
+++ install_depends.R
@@ -0,0 +1,25 @@
+depends <- c(
+  "shiny",
+  "bslib",
+  "shinyjs",
+  "tidyverse",
+  "shinyalert",
+  "zip",
+  "cowplot",
+  "rstatix",
+  "gridExtra",
+  "ggpubr",
+  "viridis",
+  "patchwork",
+  "qrcode",
+  "ggplotify",
+  "bsicons",
+  "REDCapR",
+  "scales",
+  "kableExtra",
+  "knitr",
+  "dr4pl",
+  "gt"
+)
+
+install.packages(depends)
